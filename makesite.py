715baf052f950b6506b8139afe7157ee9f6121ef
timothytrippel <trippel@umich.edu>
2021-01-08 16:34:18 -0500

    Updated license headers.
    Signed-off-by: timothytrippel <trippel@umich.edu>

--- makesite.py
+++ makesite.py
@@ -2,7 +2,7 @@
 
 # BSD 3-Clause License
 #
-# Copyright (c) 2020, Timothy Trippel <trippel@umich.edu>
+# Copyright (c) 2021, Timothy Trippel <trippel@umich.edu>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
